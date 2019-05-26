583922b147b9f8c09469bb8704bcad2c5655a005
kreis <tistewig@gmail.com>
2019-05-26 12:05:06 +0900

    スプラッシュ画面の実装 初回起動判断処理の実装 修正 修正２ データベース実装編集 入力・保存・呼び出し表示の機能実装 入力・保存・呼び出し表示の機能実装2 更新

---
+++ app/src/main/java/torach/java_conf/gr/jp/carmaintenance/DataListActivity.java
@@ -0,0 +1,25 @@
+package torach.java_conf.gr.jp.carmaintenance;
+
+import android.graphics.Color;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class DataListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_list);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolbarLayout = findViewById(R.id.toolbarLayout);
+        toolbarLayout.setTitle(getString(R.string.toolbar_title));
+        toolbarLayout.setExpandedTitleColor(Color.WHITE);
+        toolbarLayout.setCollapsedTitleTextColor(Color.LTGRAY);
+
+    }
+}
