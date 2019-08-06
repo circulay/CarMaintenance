4fa903e358f3817360927ac02e12771ec84606de
kreis <tistewig@gmail.com>
2019-08-06 18:10:39 +0900

    スプラッシュ画面の実装 初回起動判断処理の実装 修正 修正２ データベース実装編集 入力・保存・呼び出し表示の機能実装 入力・保存・呼び出し表示の機能実装2 更新 コメント追加（ツールバー処理） MaintenaceDataHelperクラス作成・ファイル名変更 再度push push test push test 2 push test 3 push test 4 push test 5 maintenance_data_show.xmlにListView追加 MaintenanceDataHelper（テーブル作成） データ入力画面作成 リストビューまでプロトタイプ作成 変数cursorの宣言場所を変更

--- app/src/main/java/torach/java_conf/gr/jp/carmaintenance/MaintenanceDataShow.java
+++ app/src/main/java/torach/java_conf/gr/jp/carmaintenance/MaintenanceDataShow.java
@@ -2,6 +2,7 @@ package torach.java_conf.gr.jp.carmaintenance;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -28,6 +29,7 @@ public class MaintenanceDataShow extends AppCompatActivity {
 
     private SimpleCursorAdapter adapter;
     private MaintenanceDataHelper helper;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,11 +60,13 @@ public class MaintenanceDataShow extends AppCompatActivity {
         SQLiteDatabase db = helper.getReadableDatabase();
 
         try {
+
             // テーブル内の全ての項目をString[]の配列columnsに渡す
             String[] columns = new String[]{MaintenanceDataHelper.ROW_ID, MaintenanceDataHelper.ROW_CATEGORY, MaintenanceDataHelper.ROW_NAME, MaintenanceDataHelper.ROW_PRICE};
+
             // SELECT文を簡単にしてくれるメソッド "address":テーブル名, columns:項目,
             //取得したカーソルをカーソル用のアダプターに設定する。
-            Cursor cursor = db.query(MaintenanceDataHelper.TABLE_NAME, columns, null, null, null, null, null);
+            cursor = db.query(MaintenanceDataHelper.TABLE_NAME, columns, null, null, null, null, null);
 
             //cursor.moveToFirst();
 
@@ -75,7 +79,7 @@ public class MaintenanceDataShow extends AppCompatActivity {
             listView.setAdapter(adapter);
 
         } finally {
-
+            //cursor.close();
             db.close();
         }
     }
